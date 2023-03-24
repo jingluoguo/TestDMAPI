82c2630a89dc88a9873293c5d97b5b89b8249742
jingluo <55196987+jingluoguo@users.noreply.github.com>
2023-03-24 15:45:18 +0800

    Update Constant.java

--- DM_API_Module/src/main/java/com/jingluo/dm_api_module/constant/Constant.java
+++ DM_API_Module/src/main/java/com/jingluo/dm_api_module/constant/Constant.java
@@ -2,22 +2,22 @@ package com.jingluo.dm_api_module.constant;
 
 public class Constant {
     // 在云小微开放平台生成的appkey
-    public static final String DEFAULT_APP_KEY = "0f0ac2108d6511ed9d5713d974e7546d";
+    public static final String DEFAULT_APP_KEY = "";
 
     // APP Secret用于厂商账号登录，在云小微开放平台勾选厂商账号登录后，会显示出来
-    public static final String DEFAULT_APP_SECRET = "40b04ec1-bccc-11ed-9476-25eddb0b7ece";
+    public static final String DEFAULT_APP_SECRET = "";
 
     // 在云小微开放平台生成的access Token
-    public static final String DEFAULT_APP_ACCESS_TOKEN = "85174648d683477aafcbc568b368066a";
+    public static final String DEFAULT_APP_ACCESS_TOKEN = "";
 
     // 向QQ音乐部门申请到的AppID
-    public static final String QQ_MUSIC_APP_ID = "2000000372";
+    public static final String QQ_MUSIC_APP_ID = "";
 
     // 私钥，用于使用QQ音乐APP进行授权的场景
-    public static final String AppPrivateKey = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAM1iMF3WgasqDyyJG4yRxcDxFq4uvcL9JPuqeO8B5TVPoEMJJ0M5fxS1OeSeFbcZJmdHyT4qg+IaeqVBYfeGFQER0LFNwPfysqxv+/f3QxNwWdXHpisexM91mXbXMDGzpl3Kz1+jEEAMxwCbVZS1qZeuZsk9kF2UqkCliubXKSJAgMBAAECgYBTriPit9lRMevIy3wsA+C8wf3priWm4plE6KopR25qEC1N4tbabvy2JTiXS0mxtk+NWRRHfe8KGpvJ5R/TSBAaCkYRy2PAK7EWrQ5CsiPJoc6IajFx0RVNz2SETJicchgbR8srVgCMtEf4VqoXpGPEuxeQ1+JV+K971tMRp2P8uQJBAPN6CwI95/pVgJtOTQG9yc3t7F3a4TjZRCMnw1v41lHk3R6jMmw3mPg92N00MrN5WaP8QlEpxihK6jr1nRfaVpsCQQDX8o68DVC/TrXFe66Z6rO1wzzDrEGjTvxA85bstxDqBrIC/T8oYfAGEo4cqs2ergh/fL+W2cB6GUQKTsXAVZGrAkEAkUhnvUS3YbcXcJS+eqWG/6xS+IYdElECyX92oTDQojlQgHGdKtnaG3a8KvJ0todYp/Nklw24Ql7BecEJpiwQtQJBAMPHTFCBYeuPWIyqDSnN9w+a6cQcr4V3XoskTHhfoB4L4+eB4XijEhDvUZf74l8yONIcLzZ8RFxt+5m8wNwWt/ECQDA3+8bfxCsc+sb3c9cWnoCC1zWV6p8z6pPyzhFb0V82PduzzSjQlqZOF4F86J8HZE+FaDldYKE9W4AWotvly4M=";
+    public static final String AppPrivateKey = "";
 
     // productId
-    public static final String ProductId = "0f0ac2108d6511ed9d5713d974e7546d:85174648d683477aafcbc568b368066a";
+    public static final String ProductId = "";
 
     /// SP key
     // 用户授权AccessToken信息
